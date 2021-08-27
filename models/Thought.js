4ace1d987a3765e48d0ea82e15db428316d2a9ae
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-08-26 19:50:16 -0600

    Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,41 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String
+    },
+    username: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+)
+const ThoughtSchema = new Schema(
+    {
+
+    }
+)
+
+
+// create the Tought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
