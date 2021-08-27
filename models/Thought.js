a732159c7f75a59ef923ee6c007e26a4803ddaad
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-08-26 20:01:31 -0600

    Thought Model done

--- models/Thought.js
+++ models/Thought.js
@@ -29,10 +29,37 @@ const ReactionSchema = new Schema({
 )
 const ThoughtSchema = new Schema(
     {
-
+        thoughtText: {
+            type: String,
+            required: true,
+            validate: {
+                minlenght: 1,
+                maxlenght: 200
+            }
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: [
+            {
+                type:Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-)
-
+);
+ThoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length
+});
 
 // create the Tought model using the ThoughtSchema
 const Thought = model('Thought', ThoughtSchema);
